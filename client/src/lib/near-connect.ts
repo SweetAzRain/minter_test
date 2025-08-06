d99ed2e44e02fb208ec5cf02e496099225ab5374
Qelmi <33161152+SweetAzRain@users.noreply.github.com>
2025-08-06 20:11:20 +0300

    Update near-connect.ts

--- client/src/lib/near-connect.ts
+++ client/src/lib/near-connect.ts
@@ -1,6 +1,6 @@
 // Import the @hot-labs/near-connect library
 import { WalletSelector as NearWalletSelector, WalletSelectorUI as NearWalletSelectorUI } from "@hot-labs/near-connect";
-import "@hot-labs/near-connect/modal-ui.css";
+//import "@hot-labs/near-connect/modal-ui.css";
 
 // Export the classes for use in the application
 export const WalletSelector = NearWalletSelector;
